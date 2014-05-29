f58eb403f10048b19971f86fda150a512b819f01
snape james <snape@snapes-MacBook-Air.local>
2014-05-28 17:00:19 -0700

    path_sum2

---
+++ tree/lc_path_sum_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "build.h"
+
+void print_array(int path[], int len)
+{
+    for (int i = 0; i < len; i++)
+	    printf("%d ", path[i]);
+    printf("\n");
+}
+
+
+void path_sum2(node_t* root, int sum, int *path, int level)
+{
+    if (root == NULL) return;
+
+    path[level] = root->data;
+    level++;
+    sum -= root->data;
+
+    if (!root->left && !root->right && !sum)
+	    print_array(path, level);
+
+    path_sum2(root->left, sum, path, level);
+    path_sum2(root->right, sum, path, level);
+}
+
+int main()
+{
+    node_t* root = build_n_tree(100, 1);
+    int path[200] = {};
+    path_sum2(root, 50, path, 0);
+}
